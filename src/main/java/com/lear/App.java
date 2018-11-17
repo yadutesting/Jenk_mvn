9b2004abbff8302b62f7012b58b1cbec338e2c65
yadutesting <yadu.testing@gmail.com>
2018-11-18 02:30:07 +0800

    adding mvn code

---
+++ src/main/java/com/lear/App.java
@@ -0,0 +1,13 @@
+package com.lear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
